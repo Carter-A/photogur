66dbee2aee4123bd512258fcbeb3f0a223474108
Carter-A <carter.s.andrew@gmail.com>
2015-11-13 16:44:18 -0500

    protected twitter keys. Implemented Material lite.

--- config/initializers/twitter_cred.rb
+++ config/initializers/twitter_cred.rb
@@ -1,6 +1,6 @@
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key = '2l6S9CSAFVhxvlWYwxg4xFxS9'
-  config.consumer_secret = 'BogCEyxosrbNLP6NtXWu0R7EA6tQaCssuNpe3A7m2GAS31LSNs'
+  config.consumer_key = 'Bf6aZjEfyv7LYHwFLxXTyNbs5'
+  config.consumer_secret = 'MUcJlLf8PVJSw6IxVfh6dAWNKQc1lie92d1Zl8UxT1YxEItNv8'
   config.access_token = '20353440-ZK40KpY17liy5kjspbRQBe8db6wO6DIX4CLIJHCNa'
   config.access_token_secret = 'LcprSr42DQx3dc3xd4tMSB1JchfZbPPbmC0Nc4nQI35Te'
 end
